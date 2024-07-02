574a7b78097f5839fb0d404aab6f80227648f4a5
dhanushika <dhanushika76@gmail.com>
2024-07-02 09:49:28 +0530

    update

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,4 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
