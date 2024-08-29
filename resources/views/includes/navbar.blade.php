619d79e057f8eb1cca0b0d8b4099387a14ce32d1
Hirushan921 <himashahiru921@gmail.com>
2024-08-29 13:07:20 +0530

    profile

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -1,25 +1,25 @@
 <nav class="navbar navbar-expand-lg navbar-light header-sticky shadow-sm">
-     <a class="navbar-brand" href="{{ route('home') }}">
+    <a class="navbar-brand" href="{{ route('home') }}">
         <img src="{{ asset('images/logos/' . app(\App\Http\Controllers\CompanySettingController::class)->getCompanyLogo()) }}"
-        style="width:auto; height:35px;" class="mt-4" alt="Company Logo">
+            style="width:auto; height:35px;" class="mt-4" alt="Company Logo">
     </a>
-        <div class="d-flex order-lg-last">
+    <div class="d-flex order-lg-last">
         <ul class="navbar-right">
-            <div class="mt-4 btn-container" style="margin-right:50px; ">
+            <div class="mt-4 btn-container" style="margin-right:20px; ">
+                <a href="{{ route('goToProfile') }}" class="btn btn-primary btn-custom" style="padding: 7px 10px;color: #fff;font-size:12px;">PROFILE</a>
+            </div> 
+            <div class="mt-4 btn-container" style="margin-right:20px; ">
                 <a href="{{ route('login') }}" class="btn btn-primary btn-custom" style="padding: 7px 10px;color: #fff;font-size:12px;">SIGN IN</a>
             </div>
-            <div class="mt-4 btn-container" style="margin-right:50px; ">
-                <a href="{{ route('register') }}" class="btn btn-primary btn-custom" style="padding: 7px 10px;color: #fff;font-size:12px;">JOIN US</a>
-            </div>
-
+        
         </ul>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-        </div>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+    </div>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
             <li class="nav-item">
                 <a class="nav-link active" aria-current="page" href="{{ route('welcome') }}">Entrance</a>
             </li>
@@ -33,7 +33,7 @@
             <li class="nav-item">
                 <a class="nav-link active" aria-current="page" href="{{ route('store') }}">Online Store</a>
             </li>
-            </ul>
-        </div>
-    </nav>
+        </ul>
+    </div>
+</nav>
 <!-- /.End of navbar -->
